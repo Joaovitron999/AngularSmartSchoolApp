98c7b7bf6ebf8e196f464fe973f0fabad31039c4
João Vitor Fonseca <jv.fonseca999@gmail.com>
2023-01-25 21:35:00 -0300

    Routes

--- Angular/SmartSchool/src/app/alunos/alunos.component.ts
+++ Angular/SmartSchool/src/app/alunos/alunos.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-alunos',
   templateUrl: './alunos.component.html',
-  styleUrls: ['./alunos.component.css']
+  styleUrls: ['./alunos.component.css'],
 })
 export class AlunosComponent implements OnInit {
+  title = 'Alunos';
+  public alunos = [
+    { nome:'Marta'},
+    { nome:'Luana'},
+    { nome:'Marco'},
+    { nome:'Jonny'},
+    { nome:'Julia'},
+    { nome:'Paula'},
+    { nome:'Pedro'},
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- Angular/SmartSchool/src/app/professores/professores.component.ts
+++ Angular/SmartSchool/src/app/professores/professores.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professores.component.css']
 })
 export class ProfessoresComponent implements OnInit {
+  title = 'Professores';
+  public professores = [ 
+    { nome:'Ana'},
+    { nome:'Alice'},
+    { nome:'Alberto'},
+    { nome:'Amanda'},
+    { nome:'Arthur'},
+    { nome:'Aline'},
+    { nome:'Alessandro'},
+  ];
 
   constructor() { }
 
